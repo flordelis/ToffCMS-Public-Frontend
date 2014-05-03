2feb3e26b2cd42673c1ff44da9153eb39ce6e4eb
Matiss Janis Aboltins <matiss.ja@gmail.com>
2014-05-03 07:29:29 +0300

    Various bugfixes

---
+++ app/scripts/controllers/sidebar.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('publicApp')
+  .controller('SidebarCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- app/scripts/services/navigation.js
+++ app/scripts/services/navigation.js
@@ -1,5 +1,5 @@
 'use strict';
 
 app.factory('Navigation', function ($resource) {
-  return $resource('http://api.historymakers.lv/v1.0/navigation');
+  return $resource('http://api.historymakers.lv/v1.0/navigation/:language');
 });
